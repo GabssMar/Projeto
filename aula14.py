f017fb06baf8b6b17b7c91fcab0575a17dd7d0ab
Gabriele Martinez <gabimarti099@hotmail.com>
2023-11-23 21:18:11 -0300

    Projeto

--- aula14.py
+++ aula14.py
@@ -1,7 +1,7 @@
 a = 'A'
 b = 'B'
 c = 1.1
-string = a={} b={} c={:.2f} {}'
+string = 'a={0} b={1} c={2:.2f}' # <- indices
 formato = string.format(a, b, c) # <- argumentos dentro da função format
 
 print(formato)
