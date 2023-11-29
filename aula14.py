8795144bdd9c65440e2a4a2e92ca03c25d85600c
Gabriele Martinez <gabimarti099@hotmail.com>
2023-11-29 19:34:20 -0300

    projeto

--- aula14.py
+++ aula14.py
@@ -2,6 +2,6 @@ a = 'A'
 b = 'B'
 c = 1.1
 string = 'a={0} b={1} c={2:.2f}' # <- indices
-formato = string.format(a, b, c) # <- argumentos dentro da função format
+formato = string.format(nome1=a, nome2=b, nome3=c) # <- argumentos dentro da função format e parâmetros nomeados
 
 print(formato)
